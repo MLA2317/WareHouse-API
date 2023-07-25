af1a7049b35bd312b408f6a854e9b22249ae69a3
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2023-07-25 12:13:58 +0500

    Order, Cart, CartItem Done

--- warehouse/views.py
+++ warehouse/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets, serializers
+from rest_framework import generics, viewsets, serializers, permissions
 from .models import Warehouse, Cart, CartItem, Order
 from product.models import ProductItem
 from .serializer import WarehouseSerializer, CartGETSerializer, CartPostSerializer, CartItemGETSerializer, CartItemPOSTSerializer, \
@@ -9,52 +9,62 @@ from .serializer import WarehouseSerializer, CartGETSerializer, CartPostSerializ
 class WarehouseViewSet(viewsets.ModelViewSet):
     queryset = Warehouse.objects.all()
     serializer_class = WarehouseSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CartList(generics.ListAPIView):
     queryset = Cart.objects.all()
     serializer_class = CartGETSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CartCreate(generics.CreateAPIView):
     queryset = Cart.objects.all()
     serializer_class = CartPostSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CartRUD(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cart.objects.all()
     serializer_class = CartPostSerializer
     lookup_field = 'pk'
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CartItemList(generics.ListAPIView):
     queryset = CartItem.objects.all()
     serializer_class = CartItemGETSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CartItemCreate(generics.CreateAPIView):
     queryset = CartItem.objects.all()
     serializer_class = CartItemPOSTSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class CartItemRUD(generics.RetrieveUpdateDestroyAPIView):
     queryset = CartItem.objects.all()
     serializer_class = CartItemPOSTSerializer
     lookup_field = 'pk'
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class OrderList(generics.ListAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderGETSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class OrderCreate(generics.CreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderPostSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class OrderRUD(generics.RetrieveUpdateDestroyAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderPostSerializer
     lookup_field = 'pk'
+    permission_classes = [permissions.IsAuthenticated]
 
