333f283db5b762846cd107c1eed5376bedd10fed
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2023-08-07 14:56:22 +0500

    Account Admin Done

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Account
+from .models import Account, MyProduct
+from .forms import AccountChangeForms, AccountFormsCreate
 
-admin.site.register(Account)
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    add_form = AccountFormsCreate
+    form = AccountChangeForms
+    list_display = ['id', 'username', 'image_tag', 'bio', 'gender', 'role', 'is_superuser',
+                    'is_staff', 'is_active', 'is_seller', 'is_customer', 'created_date', 'modified_date']
+
+
+@admin.register(MyProduct)
+class MyHistoryJobAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'products', 'created_date']
