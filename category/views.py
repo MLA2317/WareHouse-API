514c56f917f2cd6a066d7b36602dd028e5cf80c9
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2023-08-07 15:51:38 +0500

    Category View Done

--- category/views.py
+++ category/views.py
@@ -1,10 +1,10 @@
-# from django.shortcuts import render
-# from rest_framework import generics, status, permissions
-# from .models import Category
-# from .serializer import CategorySerializer
-#
-#
-# class CategoryListCreate(generics.ListCreateAPIView):
-#     queryset = Category.objects.all()
-#     serializer_class = CategorySerializer
-#     permissions = [permissions.IsAuthenticated]
+from django.shortcuts import render
+from rest_framework import generics, status, permissions
+from .models import Category
+from .serializer import CategorySerializer
+
+
+class CategoryListCreate(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permissions = [permissions.IsAuthenticated]
