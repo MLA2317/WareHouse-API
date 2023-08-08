1023b0563ef2cd253d1b18f1c1bb8f95d7f3f233
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2023-08-08 15:11:48 +0500

    Product Serializer Done

--- product/serializer.py
+++ product/serializer.py
@@ -1,25 +1,25 @@
-# from rest_framework import serializers
-# from product.models import Product, ProductImage
-#
-#
-# class ProductImageSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = ProductImage
-#         fields = ('id', 'product', 'prod_img')
-#
-#
-# class ProductGETSerializer(serializers.ModelSerializer):
-#     images = ProductImageSerializer(many=True, read_only=True)
-#
-#     class Meta:
-#         model = Product
-#         fields = ('id', 'name', 'brand', 'description', 'images', 'choice', 'price', 'category_id', 'quantity', 'total_price', 'created_date')
-#
-#
-# class ProductPOSTSerializer(serializers.ModelSerializer):
-#     # images = ProductImageSerializer(many=True, required=False)
-#
-#     class Meta:
-#         model = Product
-#         fields = ('id', 'name', 'brand', 'description', 'images', 'choice', 'price', 'category_id', 'quantity',)
-#
+from rest_framework import serializers
+from product.models import Product, ProductImage
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ('id', 'product', 'prod_img')
+
+
+class ProductGETSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'brand', 'description', 'images', 'choice', 'price', 'category_id', 'quantity', 'total_price', 'created_date')
+
+
+class ProductPOSTSerializer(serializers.ModelSerializer):
+    # images = ProductImageSerializer(many=True, required=False)
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'brand', 'description', 'images', 'choice', 'price', 'category_id', 'quantity',)
+
