23657d9bdf0fd91a09fd8675ac67ab3929c7780e
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2023-08-08 11:18:19 +0500

    Product View done

--- product/views.py
+++ product/views.py
@@ -1,31 +1,31 @@
-# from django.shortcuts import render
-# from rest_framework import generics, status, permissions, views
-# from rest_framework.exceptions import ValidationError
-# from rest_framework.response import Response
-# from product.models import Product, ProductImage
-# from .serializer import ProductGETSerializer, ProductPOSTSerializer, ProductImageSerializer
-#
-#
-# class ProductList(generics.ListAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductGETSerializer
-#     permissions = [permissions.IsAdminUser]
-#
-#
-# class ProductCreate(generics.CreateAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductPOSTSerializer
-#     permission_classes = [permissions.IsAdminUser]
-#
-#
-# class ProductRUD(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductGETSerializer
-#     permissions = [permissions.IsAdminUser]
-#
-#
-# class ProductImageList(generics.ListAPIView):
-#     queryset = ProductImage.objects.all()
-#     serializer_class = ProductImageSerializer
-#     permission_classes = permissions.IsAuthenticated
-#
+from django.shortcuts import render
+from rest_framework import generics, status, permissions, views
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from product.models import Product, ProductImage
+from .serializer import ProductGETSerializer, ProductPOSTSerializer, ProductImageSerializer
+
+
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductGETSerializer
+    permissions = [permissions.IsAdminUser]
+
+
+class ProductCreate(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductPOSTSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class ProductRUD(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductGETSerializer
+    permissions = [permissions.IsAdminUser]
+
+
+class ProductImageList(generics.ListAPIView):
+    queryset = ProductImage.objects.all()
+    serializer_class = ProductImageSerializer
+    permission_classes = permissions.IsAuthenticated
+
