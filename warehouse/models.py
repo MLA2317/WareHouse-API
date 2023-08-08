3704efd60e51b3c624be60c94148dcfd4b55ee8d
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2023-08-08 13:06:53 +0500

    Warehouse Admin Done

--- warehouse/models.py
+++ warehouse/models.py
@@ -5,13 +5,23 @@ from category.models import Category
 from product.models import Product
 
 
+class Location(models.Model):
+    title = models.CharField(max_length=220)
+
+    def __str__(self):
+        return self.title
+
+
 class Warehouse(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, related_name='warehouse_prod')
     phone_number = models.CharField(max_length=16, verbose_name='Phone Number')
     email = models.EmailField()
-    location = models.CharField(max_length=125)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
     enter_date = models.DateTimeField(auto_now_add=True)
+
+    def product_names(self):
+        return ", ".join([str(p) for p in self.products.all()])
 
     def __str__(self):
         return f'{self.user} - {self.products} - {self.phone_number} - {self.email} - {self.location}'
@@ -26,6 +36,9 @@ class Order(models.Model):
     warehouse_id = models.ManyToManyField(Warehouse, related_name='order_warehouse')
     customer_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     status = models.CharField(max_length=7, choices=STATUS)
+
+    def warehouse_names(self):
+        return ", ".join([str(w) for w in self.warehouse_id.all()])
 
     def __str__(self):
         return f'order of {self.customer_id} (id: {self.id}) || {self.warehouse_id}'
